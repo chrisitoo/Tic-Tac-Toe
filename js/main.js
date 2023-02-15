80ae112e2bb115202d4e7abe9e822842886ca054
Chris Matute <chrisito1127@gmail.com>
2023-02-15 11:05:20 -0500

    Step 3: Make your Mark

--- js/main.js
+++ js/main.js
@@ -1,1 +1,39 @@
-console.log('Your JS is linked up. Be the person you needed when you were little.')
+console.log('Your JS is linked up. Be the person you needed when you were little.');
+
+/*----- constants -----*/
+/*----- app's state (variables) -----*/
+let board;
+
+
+
+/*----- cached element references -----*/
+
+const squares = Array.from(document.querySelectorAll('#board div'));
+
+
+
+/*----- event listeners -----*/
+    
+/*----- functions -----*/
+function init() {
+    board = [
+    'Make your Mark', '', '',
+    '', '', '',
+    '', '', ''
+    ];
+
+    render();
+
+    };
+    
+ function render() {
+        board.forEach(function(mark, index){
+       
+        squares[index].textContent = mark;
+        });
+        };
+
+    
+    
+    
+    init();
